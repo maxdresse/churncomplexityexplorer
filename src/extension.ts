5da8f5311a19bf9f6ddbcfdf80f3218f016ed021
Maximilian Dresse <max.dresse@gmx.de>
2024-11-30 20:13:35 +0100

    better folder structure

--- src/extension.ts
+++ src/extension.ts
@@ -1,8 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { WorkspaceTreeProvider } from './explorer';
-import { ControlsWebViewProvider } from './controls';
+import { WorkspaceTreeProvider } from './views/explorer';
+import { ControlsWebViewProvider } from './views/controls';
 import { ComputeChurnCommand } from './commands/compute-churn';
 
 // This method is called when your extension is activated
