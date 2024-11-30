cbc9f154ba714da569d535f4124552356b0076a8
Maximilian Dresse <max.dresse@gmx.de>
2024-11-30 20:47:56 +0100

    first running  version of command

--- src/commands/compute-churn.ts
+++ src/commands/compute-churn.ts
@@ -5,25 +5,38 @@ export class ComputeChurnCommand {
     static id = 'churncomplexityexplorer.computeChurn';
 
     async execute() {
-        async () => {
-            await vscode.window.withProgress(
-                {
-                    location: vscode.ProgressLocation.Notification,
-                    title: "Computing churn...",
-                    cancellable: true,
-                },
-                async (progress, token) => {
-                    for (let i = 0; i <= 100; i++) {
-                        if (token.isCancellationRequested) {
-                            break;
-                        }
-                        progress.report({ increment: 1, message: `Progress: ${i}%` });
-                        await new Promise(resolve => setTimeout(resolve, 200));
-                    }
-                    vscode.window.showInformationMessage("Task completed!");
-                }
-            );
-        }
+        await vscode.window.withProgress({
+			location: vscode.ProgressLocation.Notification,
+			title: "I am long running!",
+			cancellable: true
+		}, (progress, token) => {
+			token.onCancellationRequested(() => {
+				console.log("User canceled the long running operation");
+			});
+
+			progress.report({ increment: 0 });
+
+			setTimeout(() => {
+				progress.report({ increment: 10, message: "I am long running! - still going..." });
+			}, 1000);
+
+			setTimeout(() => {
+				progress.report({ increment: 40, message: "I am long running! - still going even more..." });
+			}, 2000);
+
+			setTimeout(() => {
+				progress.report({ increment: 50, message: "I am long running! - almost there..." });
+			}, 3000);
+
+			const p = new Promise<void>(resolve => {
+				setTimeout(() => {
+					resolve();
+				}, 5000);
+			});
+
+			return p;
+		});
+        
     }
 
 }
