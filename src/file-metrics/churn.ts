3c67d45b36370c64de7e3b7574011bb2d376b585
Maximilian Dresse <max.dresse@gmx.de>
2024-12-01 13:40:17 +0100

    rename class

--- src/file-metrics/churn.ts
+++ src/file-metrics/churn.ts
@@ -4,7 +4,7 @@ import { execRawGitCommand } from '../git';
 // git log --all --find-renames --find-copies --name-only --format=format: --since="2 years ago" --until=HEAD LICENSE
 const since = "2 years ago";
 
-export class AbsoluteChurnCount {
+export class AbsoluteChurnCounter {
     // adapted from 
     // https://github.com/CarlosBonetti/vscode-uplift-code/blob/d69fe5147519ba02d492bf60bf22e76093111966/src/metrics/git/churn.ts
     async init(git: SimpleGit) {
