fcf62b9b8f6836bc6f1b322378c110539715c542
Maximilian Dresse <max.dresse@gmx.de>
2024-12-04 07:25:45 +0100

    refactor

--- src/commands/compute-churn.ts
+++ src/commands/compute-churn.ts
@@ -5,6 +5,11 @@ import { AbsoluteChurnCounter } from '../file-metrics/absolute-churn-counter';
 import { FileMetric } from '../file-metrics/file-metric';
 import { StorageAccess } from '../persistence/storage-access';
 
+const progressTitle = "Computing project churn. ";
+const msgRetrieveGitLogs = "Retrieving git logs";
+const msgProcessing = "Processing results";
+const msgSaving = "Saving results";
+
 export class ComputeChurnCommand {
 
     static id = 'churncomplexityexplorer.computeChurn';
@@ -14,18 +19,18 @@ export class ComputeChurnCommand {
     async execute() {
         await vscode.window.withProgress({
 			location: vscode.ProgressLocation.Notification,
-			title: "Computing project churn. ",
+			title: progressTitle,
 			cancellable: true
 		}, async (progress, token) => {
 			token.onCancellationRequested(() => {
-				console.log("User canceled the long running operation");
+				console.log("User canceled the computation");
 			});
-			progress.report({ increment: 0, message: "Retrieving git logs" });
+			progress.report({ increment: 0, message: msgRetrieveGitLogs });
 			const counter = new AbsoluteChurnCounter();
 			await counter.init();
-			progress.report({ increment: 50, message: "Processing results" });
+			progress.report({ increment: 50, message: msgProcessing });
 			const metric = FileMetric.extendRegularFileMetricByMax(counter);
-			progress.report({ increment: 80, message: "Saving results" });
+			progress.report({ increment: 80, message: msgSaving });
 			const storageAccess = new StorageAccess(this.context);
 			storageAccess.save("testdata.json", metric.serialize());
 			});
