709f7078fb9151f0972e9b0815972004c838d7e4
Maximilian Dresse <max.dresse@gmx.de>
2024-12-06 14:25:28 +0100

    generic combine function

--- src/views/label-decorator.ts
+++ src/views/label-decorator.ts
@@ -1,3 +1,7 @@
 export type LabelDecorator = (label: string, absoluteFilePath: string) => string;
 
 export type LabelDecoratorFactory = () => Array<LabelDecorator>;
+
+export function combineDecoratorFactories(factories: Array<LabelDecoratorFactory>): LabelDecoratorFactory {
+    return () => factories.flatMap(factory => factory());
+}
