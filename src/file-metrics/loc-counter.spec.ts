aad8e2654eeceeb2116dd22c7e9018a37ef3fb5a
Maximilian Dresse <max.dresse@gmx.de>
2024-12-28 13:40:44 +0100

    first meaningful jest test

--- src/file-metrics/loc-counter.spec.ts
+++ src/file-metrics/loc-counter.spec.ts
@@ -1,5 +1,34 @@
+jest.mock('fs', () => ({ readFileSync: jest.fn(() => `
+    import { abc } from 'def';
+
+    export class A {
+        constructor() {
+            console.log('Hello, World!');
+        }
+    }
+    `) })
+);
+import { LocCounter } from './loc-counter';
+import { workspace, Uri, type WorkspaceFolder } from 'vscode';
 describe('loc-counter', () => {
-    it('sanity check', () => {
-        expect(1).toBe(1);    
+
+    beforeEach(() => {
+        jest.clearAllMocks();
     });
+
+    it("should correctly count the lines of code for a .ts file", () => {
+        const filePath = 'myboguspath.ts';
+        const rootUri = Uri.file('/');
+        const workspaceFolder1: WorkspaceFolder = {
+        uri: Uri.joinPath(rootUri, 'Folder1'),
+        name: 'Folder1',
+        index: 0,
+        };
+        const spy = jest.spyOn(workspace, 'workspaceFolders', 'get');
+        spy.mockReturnValue([workspaceFolder1]);
+        
+        const counter = new LocCounter();
+        const count = counter.getValue(filePath)
+        expect(count).toBe(6);
+    })
 });
