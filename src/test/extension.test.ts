48592c3d5a327cf3c1d97df24a9e9420a9e7e207
Maximilian Dresse <max.dresse@gmx.de>
2025-01-06 13:51:41 +0100

    steps towards e2e smoke test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,14 +1,18 @@
 import * as assert from 'assert';
+import path from 'path';
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+export const churnComplexityRepoPath = path.resolve(__dirname, '..', '..');
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
+	test('Complexity smoke test', async () => {
+		await vscode.commands.executeCommand('churncomplexityexplorer.computeLoc');
+		//vscode.window.createTreeView('cc-explorer', { treeDataProvider: new CCExplorerProvider() });
 		assert.strictEqual(1, 1);
 	});
 });
